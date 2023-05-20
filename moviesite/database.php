5c181069b1f239621d06e05a35d83d78d3ce1e31
Borislav Pavlov <bpavlov123bp@gmail.com>
2023-05-20 22:36:55 +0300

    Add files via upload

--- moviesite/database.php
+++ moviesite/database.php
@@ -1,24 +1,36 @@
 <?php
+require_once('moviesite_view.php');
 class Database{
     private $db_host = 'localhost';
     private $db_user = 'bp5am';
     private $db_pass = 'bp5ampass';
     private $db_name = 'moviesite';
     private $conn;
-
     public function __construct()
     {
+        
         $this->conn = mysqli_connect($this->db_host, $this-> db_user, $this->db_pass) or die(mysqli_error($this->conn));  
         mysqli_select_db($this->conn, $this->db_name) or die(mysqli_error($this->conn));  
     }
-    public function select(){
+    public function selectMovie(){
         $result = $this->conn->query("SELECT 
-        movie_name, movietype_label 
-        FROM movie 
-        LEFT JOIN movietype 
-        ON movie_type = movietype_id 
-        WHERE movie.movie_year>1990 
-        ORDER BY movie_type");
+        movie_name, movie_director, movie_leadactor 
+        FROM movie");
         return $result;
     }
+    public function countMovies(){
+        $count_movies = mysqli_num_rows($this->selectMovie());
+        return $count_movies;
+    }
+    public function getDirector(){
+        $resultDirector = $this->conn->query("SELECT 
+        people_fullname FROM people LEFT JOIN movie ON people.people_id = movie.movie_director");
+        return $resultDirector;
+    }
+    public function getActor(){
+        $resultActor = $this->conn->query("SELECT 
+        people_fullname FROM people LEFT JOIN movie ON people.people_id = movie.movie_leadactor");
+        return $resultActor;
+    }
 }
+
--- moviesite/moviesite_view.php
+++ moviesite/moviesite_view.php
@@ -8,15 +8,39 @@ class View{
     }
     public function output(){
         $outputData = "";
-        $result = $this->database->select();
-        $outputData .= "<table border=\"1\">\n";
-        while($row = $result->fetch_assoc()){
-           $outputData .= "<tr>\n";
-           foreach($row as $value){
-            $outputData .= "<td>\n" . $value . "</td>\n";
-           }
-           $outputData .= "</tr>";
+        $result = $this->database->selectMovie();
+        $resultDirector = $this->database->getDirector();
+        $resultActor = $this->database->getActor();
+        $outputData .= "<h2><center>Movie Review Database</center></h2>
+        <table border=\"1\" align=\"center\">
+        <tr>
+        <th>Movie Title</th>
+        <th>Movie Director</th>
+        <th>Movie Lead Actor</th>
+        </tr>";
+        
+        while($row = $result->fetch_array()){
+            $movie_name = $row['movie_name'];
+            $movie_director = $row['movie_director'];
+            $movie_leadactor = $row['movie_leadactor'];
+            $outputData .= "<tr>\n";
+            $outputData .= "<td>$movie_name</td>";
+            $outputData .= "<td>"; 
+            $row_d = $resultDirector->fetch_array();
+            extract($row_d);
+            $director = $people_fullname;
+            $outputData .= "$director</td>";
+            $outputData .= "<td>";
+            $row_a = $resultActor->fetch_array();
+            extract($row_a);
+            $leadActor = $people_fullname;
+            $outputData .= "$leadActor</td>";
         }
+       
+        $outputData .= "</tr>";
+        $outputData .= "<tr>
+        <td colspan=\"3\">Total movies: " . $this->database->countMovies() . "</td>
+        </tr>";
         $outputData .= "</table>\n";
         return $outputData;
     }
