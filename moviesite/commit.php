041771e496e1ed5abab7bd1f5f3da8cb7b66665d
Borislav Pavlov <bpavlov123bp@gmail.com>
2023-06-01 21:40:12 +0300

    Update commit.php

--- moviesite/commit.php
+++ moviesite/commit.php
@@ -1,12 +1,1 @@
-<?php
-require_once('database.php');
-class AddMovie{
-    private $database;
-    public function __construct()
-    {
-        $this->database = new Database();
-    }
-    public function addMovie(){
-        echo "Az sam";
-    }
-}
+
