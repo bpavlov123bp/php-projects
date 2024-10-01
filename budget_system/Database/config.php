8efdd98da2abe2475730f8750c4761e629c00873
Borislav Pavlov <bpavlov123bp@gmail.com>
2024-10-01 21:52:25 +0300

    Delete budget_system directory

--- budget_system/Database/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'budget');
-
-$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS)
-    or die("Check your server connection!");
-mysqli_select_db($connect, DB_NAME);    
--- budget_system/Views/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(__DIR__ . '/../Controllers/users.php');
-logout();
-header('Location: /budget/index.php');
